269da9686fc3aa1b63b24350ba100c9a860de13e
Enes Özbek <https://github.com/DanOzb>
2026-01-21 21:30:46 +0100

    Test: testing a simple counter implementation in main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,27 +1,36 @@
 #include "GameEngine.h"
 #include "Button.h"
+#include "Label.h"
 #include <memory>
 #include <iostream>
 
 /**
  * Main class. 
- * Used to test Button component now. 
+ * Used to implement a simple counter now.
  * Change Later 
  */
 
 using namespace engine;
 using namespace std;
 
+LabelPtr label; 
+int value = 0; 
 void func(string txt){
-	cout << txt << endl;
+	if(txt == "+")
+		++value; 
+	else 
+		--value;
+	label->setText(to_string(value));
 }
 
 int main() {
-	CompPtr c1 = Button::make(100, 100, 200, 75, "example 1", func);
-	CompPtr c2 = Button::make(400, 100, 200, 75, "example 2", func);
+	CompPtr c1 = Button::make(100, 100, 100, 75, "+", func);
+	CompPtr c2 = Button::make(400, 100, 100, 75, "-", func);
+	label = Label::make(250, 100, 100, 75, "0");
 
 	eng.add(c1); 
 	eng.add(c2); 
+	eng.add(label);
 
 	eng.run();
 	return 0; 
